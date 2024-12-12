2241bb2774cc432a29141890fe720a530eba1c0e
NguyenLuan11 <nguyenluan0101020@gmail.com>
2024-12-12 14:07:24 +0700

    add change pasword

--- lib/screens/settings/change_password_screen.dart
+++ lib/screens/settings/change_password_screen.dart
@@ -3,9 +3,13 @@
 import 'package:flutter/material.dart';
 import 'package:food_nutrition_app/api/services/user_service.dart';
 import 'package:food_nutrition_app/contants.dart';
+import 'package:food_nutrition_app/local_components/default_button.dart';
 import 'package:food_nutrition_app/models/user.dart';
+import 'package:food_nutrition_app/screens/home/components/bottom_navbar.dart';
+import 'package:food_nutrition_app/screens/login_register/components/PasswordField_design.dart';
 import 'package:food_nutrition_app/screens/login_register/components/show_message_dialog.dart';
 import 'package:food_nutrition_app/screens/login_register/login_screen.dart';
+import 'package:food_nutrition_app/size_config.dart';
 import 'package:food_nutrition_app/utils/crypto_pass.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -45,27 +49,35 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
 
   void updatePass() async {
     if (newPass.text == confirmPass.text) {
-      try {
-        bool isSuccess = await UserService().updatePass(
-            widget.user.userId!, encryptPass(newPass.text, appName));
+      if (newPass.text.length >= 8) {
+        try {
+          bool isSuccess = await UserService().updatePass(
+              widget.user.userId!, encryptPass(newPass.text, appName));
 
-        if (isSuccess) {
-          setState(() {
-            isCorrectPass = !isCorrectPass;
-          });
+          if (isSuccess) {
+            setState(() {
+              isCorrectPass = !isCorrectPass;
+            });
 
-          SharedPreferences pref = await SharedPreferences.getInstance();
-          pref.remove("userId");
-          pref.remove("accessToken");
+            await showMessageDialog(context, "Update Password",
+                "Your password be updated successfully!");
 
-          Navigator.of(context).pushAndRemoveUntil(
-            MaterialPageRoute(builder: (context) => const LoginScreen()),
-            (Route<dynamic> route) => false,
-          );
+            SharedPreferences pref = await SharedPreferences.getInstance();
+            pref.remove("userId");
+            pref.remove("accessToken");
+
+            Navigator.of(context).pushAndRemoveUntil(
+              MaterialPageRoute(builder: (context) => const LoginScreen()),
+              (Route<dynamic> route) => false,
+            );
+          }
+        } catch (e) {
+          await showMessageDialog(
+              context, "Update Password Failed!", e.toString());
         }
-      } catch (e) {
-        await showMessageDialog(
-            context, "Update Password Failed!", e.toString());
+      } else {
+        await showMessageDialog(context, "Update Password Failed!",
+            "Password must be longer than or equal to 8 characters!");
       }
     } else {
       await showMessageDialog(
@@ -75,8 +87,83 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
 
   @override
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    var orientation = MediaQuery.of(context).orientation;
+
     return Scaffold(
       appBar: AppBar(title: const Text("Đổi mật khẩu")),
+      bottomNavigationBar: const BottomNavbar(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: orientation == Orientation.portrait
+              ? const EdgeInsets.all(kDefaultPadding)
+              : const EdgeInsets.all(kDefaultPadding * 2),
+          child: Column(
+            children: [
+              isCorrectPass == false
+                  ? Container(
+                      padding: const EdgeInsets.all(kDefaultPadding / 1.5),
+                      height: orientation == Orientation.portrait
+                          ? SizeConfig.screenHeight * 0.19
+                          : SizeConfig.screenHeight * 0.42,
+                      width: SizeConfig.screenWidth * 0.9,
+                      decoration: BoxDecoration(
+                        color: Colors.green[100],
+                        borderRadius: const BorderRadius.all(
+                            Radius.circular(kDefaultPadding / 2)),
+                      ),
+                      child: Column(
+                        children: [
+                          PasswordFieldDesign(
+                            controller: currentPass,
+                            labelText: "Nhập mật khẩu",
+                          ),
+                          const SizedBox(height: kDefaultPadding / 2),
+                          DefaultButton(
+                              text: "Đồng ý",
+                              press: () {
+                                checkCorrectPass();
+                              },
+                              backgroundColorBtn: blueColor),
+                        ],
+                      ),
+                    )
+                  : Container(
+                      padding: const EdgeInsets.all(kDefaultPadding / 1.5),
+                      height: orientation == Orientation.portrait
+                          ? SizeConfig.screenHeight * 0.28
+                          : SizeConfig.screenHeight * 0.6,
+                      width: SizeConfig.screenWidth * 0.9,
+                      decoration: BoxDecoration(
+                        color: Colors.green[100],
+                        borderRadius: const BorderRadius.all(
+                            Radius.circular(kDefaultPadding / 2)),
+                      ),
+                      child: Column(
+                        children: [
+                          PasswordFieldDesign(
+                            controller: newPass,
+                            labelText: "Nhập mật khẩu mới",
+                          ),
+                          const SizedBox(height: kDefaultPadding / 2),
+                          PasswordFieldDesign(
+                            controller: confirmPass,
+                            labelText: "Xác nhận mật khẩu mới",
+                          ),
+                          const SizedBox(height: kDefaultPadding / 2),
+                          DefaultButton(
+                              text: "Đổi mật khẩu",
+                              press: () {
+                                updatePass();
+                              },
+                              backgroundColorBtn: redColor),
+                        ],
+                      ),
+                    ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
